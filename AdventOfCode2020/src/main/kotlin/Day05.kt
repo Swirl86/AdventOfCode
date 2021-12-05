3c08c08a3b30e201ac1da9c97e74bfd9a83574b1
Swirl86 <swesuzzi@gmail.com>
2021-12-05 14:08:52 +0100

    Day 5 and 6

--- AdventOfCode2020/src/main/kotlin/Day05.kt
+++ AdventOfCode2020/src/main/kotlin/Day05.kt
@@ -1,32 +1,77 @@
 import java.io.File
+import java.util.stream.Stream
+
 
 /*https://adventofcode.com/2020/day/5
 * What is the highest seat ID on a boarding pass?
-* part one -->
-* part two -->
+* part one --> 919
+* part two --> 642
 */
 class Day05 {
 
-    private val rows = mutableListOf<String>()
+    private val list = mutableListOf<String>()
+    private val boardingPassList = mutableListOf<Int>()
 
     fun run() {
         loadFile()
-        //partOne()
-        //partTwo()
+        partOne()
+        partTwo()
     }
 
     private fun loadFile() {
         File("data-files/Day05.txt").forEachLine {
-            rows.add(it)
+            list.add(it)
         }
     }
 
+    /* 128 Rows 8 columns
+     The first 7 characters will either be F or B
+     front (0 through 63) or the back (64 through 127)
+     last three characters will be either L or R 0 - 7
+     F and L - Lower half
+     B and R - upper half */
     private fun partOne() {
-        TODO("Not yet implemented")
+         var highestBoardingPass = 0
+
+         for (i in 0 until list.size) {
+             val rowNumber = seatIdLoop(list[i], 'F', 'B', 128)
+             val columnNumber = seatIdLoop(list[i], 'L', 'R', 8)
+             val boardingPass = (rowNumber * 8) + columnNumber
+             if(boardingPass > highestBoardingPass) highestBoardingPass = boardingPass
+
+             // For part two
+             boardingPassList.add(boardingPass)
+         }
+
+        println("Part One : $highestBoardingPass")
+    }
+
+    private fun seatIdLoop(row: String, lowHalf: Char, upperHalf: Char, range: Int): Int {
+        var upperRange = range
+        var lowerRange = 0
+
+        for (i in row.indices) {
+            val middle = (upperRange + lowerRange) / 2
+            when(row[i]) {
+                lowHalf -> upperRange = middle // F or L
+                upperHalf -> lowerRange = middle // B or R
+            }
+        }
+        return lowerRange
     }
 
     private fun partTwo() {
-        TODO("Not yet implemented")
+        boardingPassList.sort()
+        var missingSeatId = 0
+
+        Stream.iterate(1) { i -> i + 1 }
+            .takeWhile { i -> i < boardingPassList.size - 1 }
+            .forEach { i ->
+                if((boardingPassList[i] + 1) !=  boardingPassList[i+1])
+                    missingSeatId = boardingPassList[i] + 1
+            }
+
+        println("Part Two : $missingSeatId")
     }
 
 }
---
+++ AdventOfCode2020/src/main/kotlin/Day06.kt
@@ -0,0 +1,63 @@
+import java.io.File
+/*https://adventofcode.com/2020/day/6
+* For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+* part one --> 6633
+* part two --> 3202
+*/
+class Day06 {
+    private val list = mutableListOf<String>()
+    private val groups = mutableListOf<MutableList<String>>()
+
+    fun run() {
+        loadFile()
+        partOne()
+        partTwo()
+    }
+
+    private fun loadFile() {
+        File("data-files/Day06.txt").forEachLine {
+            list.add(it)
+        }
+
+    }
+
+    private fun partOne() {
+        var rowBuilder = ""
+        var sum:Long = 0
+        list.forEach { row ->
+            if (row.isEmpty()) {
+                sum += rowBuilder.chars().distinct().count()
+                rowBuilder = ""
+            } else {
+                rowBuilder += row
+            }
+        }
+        sum += rowBuilder.chars().distinct().count()
+        println("Part One : $sum")
+    }
+
+    private fun partTwo() {
+        var sum:Long = 0
+
+        val rowBuilder = mutableListOf<String>()
+        list.forEach { row ->
+            if (row.isEmpty()) {
+                sum += countYesAnswers(rowBuilder)
+                rowBuilder.clear()
+            } else {
+                rowBuilder.add(row)
+            }
+        }
+        sum += countYesAnswers(rowBuilder)
+        println("Part Two : $sum")
+    }
+
+    private fun countYesAnswers(group: MutableList<String>): Int {
+        if(group.size == 1) return group[0].length
+        // toSet - a Set of all characters
+        // reduceRight - Accumulates value starting with the last element and applying operation from right to left to each element and current accumulator value.
+        // intersect - Returns a set containing all elements that are contained by both this collection and the specified collection.
+        return group.map(String::toSet).reduceRight(Set<Char>::intersect).size
+      //  return group.sumOf { it -> it.lines().reduce{ s, t -> s.filter{ it in t } }.length}
+    }
+}
