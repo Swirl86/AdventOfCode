b489ce4aa0cda9bdc2a6da6ea02259fbdbd56dd8
Swirl86 <swesuzzi@gmail.com>
2024-11-11 08:07:42 +0100

    added year 2022 day 1

---
+++ AdventOfCode2022/Day01/part1a2.py
@@ -0,0 +1,31 @@
+"""
+https://adventofcode.com/2022/day/1
+Advent of Code 2022 Day 1: Calorie Counting
+"""
+
+with open("input.txt") as f:
+    all_elf_calories = []
+    current_elf = 0
+
+    for line in f.readlines():
+        if len(line.strip()) == 0:
+            all_elf_calories.append(current_elf)
+            current_elf = 0
+        else:
+            current_elf += int(line)
+
+    all_elf_calories.append(current_elf)
+
+    """
+    Find the Elf carrying the most Calories.
+    How many total Calories is that Elf carrying?
+    """
+    print("Part1: ", max(all_elf_calories))
+    
+
+    """
+    Find the top three Elves carrying the most Calories.
+    How many Calories are those Elves carrying in total?
+    """
+
+    print("Part2: ", sum(sorted(all_elf_calories, reverse=True)[:3]))
